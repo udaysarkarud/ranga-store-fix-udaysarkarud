41b8f45e4e9997c9d0f1ae63afdc8485afe754bf
udaysarkarud <udaysarkarud@gmail.com>
2021-09-13 02:51:55 +0600

    fixed all js bugs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
+  const url = `http://127.0.0.1:5500/ranga-api.json`;
+  //const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
@@ -18,8 +19,10 @@ const showProducts = (products) => {
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
+      <h4>Avarage Rating: ${product?.rating?.rate} | Total Rated: ${product?.rating?.count}</h4>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
@@ -32,21 +35,23 @@ const addToCart = (id, price) => {
   updatePrice("price", price);
 
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
+
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -78,3 +83,8 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+
+// Buy Now Button
+document.getElementById('buyNowBtn').addEventListener('click', () => {
+  location.reload();
+})
